d5d52ba9a5ef8d58a85f9c69d459f4eb2b5ec34d
floatn <nicholas.cheredov@gmail.com>
2016-04-12 16:53:10 +0300

    Manage site sections

--- vybor_project/urls.py
+++ vybor_project/urls.py
@@ -6,17 +6,18 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('home.urls', namespace='home')),
-    url(r'^news/', include('news.urls', namespace='news')),
-    url(r'^contacts/', include('contacts.urls', namespace='contacts')),
-    url(r'^projects/', include('projects.urls', namespace='projects')),
-    url(r'^documents/', include('documents.urls', namespace='documents')),
-    url(r'^advisors/', include('advisors.urls', namespace='advisors')),
-    url(r'^strategy/', include('strategy.urls', namespace='strategy')),
-    url(r'^partners/', include('partners.urls', namespace='partners')),
-    url(r'^video/', include('video.urls', namespace='video')),
-    url(r'^images/', include('images.urls', namespace='images')),
-]
+    url(r'^$', include('home.urls')),
+    url(r'^news/', include('news.urls')),
+    url(r'^about/strategy/', include('strategy.urls')),
+    url(r'^about/', include('contacts.urls')),
+    url(r'^contacts/', include('contacts.urls')),
+    #url(r'^projects/', include('projects.urls')),
+    url(r'^documents/', include('documents.urls')),
+    #url(r'^advisors/', include('advisors.urls')),
+    #url(r'^direction/', include('direction.urls')),
+    url(r'^partners/', include('partners.urls')),
+    url(r'^media/video/', include('video.urls')),
+    url(r'^media/images/', include('images.urls')),
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
