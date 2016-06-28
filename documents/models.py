bc629b8c0d6082cb4ccb0dcdd930d726a62afc77
floatn <nicholas.cheredov@gmail.com>
2016-06-28 14:32:04 +0300

    Restylized document part

--- documents/models.py
+++ documents/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Documents(models.Model):
     file = models.FileField(upload_to="docs/")
-    description = models.CharField(max_length=35)
+    description = models.CharField(max_length=255)#(max_length=35)
     #icon = models.ImageField(upload_to="media/icons/", blank=True, null=True)
 
     def __unicode__(self):
