c93fa3e4c9003cee7fda054d8dbe8a4cbffb7df8
floatn <nicholas.cheredov@gmail.com>
2016-06-29 13:39:47 +0300

    Document page add create_date

--- documents/views.py
+++ documents/views.py
@@ -11,6 +11,11 @@ class DocumentsPageView(generic.ListView):
         context_object_name = "document_list"
 	def get_context_data(self, **kwargs):
 		context = super(DocumentsPageView, self).get_context_data(**kwargs)
+                new_object_list = []
+                for object in context['document_list']:
+                    created_time = object.file.storage.created_time(object.file.name)
+                    new_object_list.append((object, created_time))
+                context['document_list'] = new_object_list
                 context['slide_list'] = Slides.objects.all()
 		context['nav_active_button'] = 'Documents'
 		return context
