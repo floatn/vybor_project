0f9991a1759de65cc7e89553aaea4ec819ca746d
floatn <nicholas.cheredov@gmail.com>
2016-07-06 12:51:31 +0300

    Localize 'about' app admin name

--- about/apps.py
+++ about/apps.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
 
 class AboutConfig(AppConfig):
-    name = 'about'
+    name = 'About'
+    verbose_name = 'О нас'
